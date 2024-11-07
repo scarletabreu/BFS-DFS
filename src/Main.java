c3859e11e9b0e70db8ccfb7859d813046e7110ec
Scarlet Abreu <168682311+scarletabreu@users.noreply.github.com>
2024-11-07 17:59:32 -0400

    Cambio de matriz porque no me fije bien.

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,17 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[][] matriz = {
-                {0, 1, 1, 0},
-                {1, 0, 0, 1},
-                {1, 0, 0, 1},
-                {0, 1, 1, 0}
+                {0, 1, 0, 0, 1, 0, 1, 0, 0, 0},
+                {1, 0, 1, 1, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 1, 1, 0, 0, 0},
+                {0, 1, 0, 0, 0, 1, 1, 0, 0, 0},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                {0, 0, 1, 1, 0, 0, 1, 1, 0, 0},
+                {1, 0, 1, 1, 0, 1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 1, 1, 0, 1, 1},
+                {0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+                {0, 0, 0, 0, 1, 0, 0, 1, 1, 0}
         };
-
         Graph graph = new Graph(matriz);
         System.out.println("Matriz de adyacencia:" + Arrays.deepToString(matriz));
         System.out.println("Recorrido utilizando BFS: " + graph.BFS(0));
